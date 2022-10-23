af57e2c6ae4bedd5412f9a47b00d6a9fc2f80e03
Nicholas-David Labarre <ndlabarre@coveo.com>
2022-10-23 14:44:05 -0400

    Handle absent config file

--- cmd/config.go
+++ cmd/config.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/viper"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var configFileName = ".pop"
 
 var configCmd = &cobra.Command{
 	Use:   "config",
@@ -22,11 +23,22 @@ func InitConfig() {
 	viper.AddConfigPath(home)
 	viper.AddConfigPath(".")
 	viper.SetConfigType("yaml")
-	viper.SetConfigName(".pop")
+	viper.SetConfigName(configFileName)
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println(err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			cobra.CheckErr(err)
+		}
+
+		createConfigFile()
 	}
+}
+
+func createConfigFile() {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	cobra.CheckErr(viper.SafeWriteConfigAs(home + "/" + configFileName))
 }
 
 func init() {
